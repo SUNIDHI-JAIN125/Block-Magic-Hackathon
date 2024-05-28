1423d05d25a5dec7bebb63a900da0e3d7059a41f
drex007 <vykeoj@gmail.com>
2024-05-28 06:19:54 +0100

    fixes

--- frontend/app/components/NftCard.tsx
+++ frontend/app/components/NftCard.tsx
@@ -13,9 +13,10 @@ const NftCard = () => {
          <p className='text-[10px] font-semibold'>MeeKey NFt #006</p>
         <p className='text-[10px] text-gray-300'>1400 MATIC</p>
        </div>
-        <div className='flex space-x-3   my-1'>
+        <div className='flex space-x-3 '>
         <button className='px-8 py-2 bg-primary-button text-[8px] rounded-sm font-poppins font-semibold' onClick={()=> setModalChoices({...modalChoices, buyNftLiquidityModal:true})}>Buy</button>
-            <button className='px-8 py-2 bg-red-500 text-[8px] rounded-sm font-poppins font-semibold'>Sell</button>
+        <button className='px-9 py-3 bg-red-500 text-[8px] rounded-sm font-poppins font-semibold' onClick={()=> setModalChoices({...modalChoices, buyNftLiquidityModal:true})}>Sell</button>
+          
         </div>
         
     </div>
