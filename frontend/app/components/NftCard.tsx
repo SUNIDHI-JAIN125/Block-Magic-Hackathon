ada7a4481effa7463a6aa985ce8ac5ee767a4cf4
drex007 <vykeoj@gmail.com>
2024-05-30 08:01:49 +0100

    feat: increased sidebar, buy and sell  fonts

--- frontend/app/components/NftCard.tsx
+++ frontend/app/components/NftCard.tsx
@@ -14,8 +14,8 @@ const NftCard = () => {
         <p className='text-[10px] text-gray-300'>1400 MATIC</p>
        </div>
         <div className='flex space-x-3 '>
-        <button className='px-8 py-2 bg-primary-button text-[8px] rounded-sm font-poppins font-semibold' onClick={()=> setModalChoices({...modalChoices, buyNftLiquidityModal:true})}>Buy</button>
-        <button className='px-9 py-3 bg-red-500 text-[8px] rounded-sm font-poppins font-semibold' onClick={()=> setModalChoices({...modalChoices, buyNftLiquidityModal:true})}>Sell</button>
+        <button className='px-8 py-2 bg-primary-button text-[12px] rounded-sm font-poppins font-semibold' onClick={()=> setModalChoices({...modalChoices, buyNftLiquidityModal:true})}>Buy</button>
+        <button className='px-9 py-3 bg-red-500 text-[12px] rounded-sm font-poppins font-semibold' onClick={()=> setModalChoices({...modalChoices, buyNftLiquidityModal:true})}>Sell</button>
           
         </div>
         
